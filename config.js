d392429600d409fe76427a2dba837ff98dbbe84e
harshrwt18 <67794494+harshrwt18@users.noreply.github.com>
2020-07-04 11:26:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-	"TOKEN": process.env.TOKEN,
-	"YOUTUBE_API_KEY": process.env.YOUTUBE_API_KEY,
+	"TOKEN": process.env.NzI4ODM2Njc1MDE0MjMwMDc5.XwAYaA.JVbk-taWPKT2ivFWiITihul23F8,
+	"YOUTUBE_API_KEY": process.env.AIzaSyBA2H1wBBPp8ctyl43KkO3ab_pmzJy3fMs,
 	"MAX_PLAYLIST_SIZE": 10,
-	"PREFIX": "!",
+	"PREFIX": ".",
 	"MUSIC_CHANNEL_ID": process.env.MUSIC_CHANNEL_ID
 };
